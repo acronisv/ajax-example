b3ed14f7efa5cfff1494e18b659c0fd789b5d8cf
acronisv <acronisv@gmail.com>
2021-02-16 23:11:27 +0300

    Add post, put, delete methods. Use axios instead of jquery.

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 let pageNumber = document.getElementById('page-number')
+let taskText = document.getElementById('task-text')
+let taskId = document.getElementById('task-id')
+let getTasksButton = document.querySelector('.get-tasks-btn')
+let requestButton = document.querySelector('.request-btn')
+let setTaskButton = document.querySelector('.set-task-btn')
+let deleteTaskButton = document.querySelector('.delete-task-btn')
 
 function onGetData(data) {
     data.forEach(element => {
@@ -8,10 +14,38 @@ function onGetData(data) {
     });
 }
 
-document.querySelector('.btn-ajax').addEventListener('click', function(){
-    getData(pageNumber.value, onGetData)
+function onGetTasks(array) {
+    let content = document.querySelector('.task-content')
+    content.innerHTML = ''
+    array.forEach(element =>{
+        let li = document.createElement('li')
+        li.innerHTML = element.title
+        li.appendChild(document.createElement('p')).append(element.id)
+        content.appendChild(li)
+    })
+}
+
+requestButton.addEventListener('click', function(){
+    const promise = getData(pageNumber.value)
+    promise
+        .then(onGetData)
 })
 
+getTasksButton.addEventListener('click', function() {
+    const promise = getTasks()
+    promise.then(onGetTasks)
+})
 
+setTaskButton.addEventListener('click', function(){
+    const promise = createTask(taskText.value)
+    promise.then((data)=>{
+        console.log(data)
+    })
+})
 
-
+deleteTaskButton.addEventListener('click', function(){
+    const promise = deleteTask(taskId.value)
+    promise.then(()=>{
+        console.log('deleted')
+    })
+})
--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function getData(pageNumber, callback){
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=2`, {
-        success: callback
+function getData(pageNumber){
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=2`)
+    return promise.then((response)=>{
+        return response.data
+    })
+}
+
+function getTasks(){
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=4242`)
+    return promise.then((response)=>{
+        console.log(response.data)
+        return response.data
+    })
+}
+
+function createTask(title){
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks?`,{
+        widgetId: 4242,
+        title: title
+    })
+    return promise.then((response)=>{
+        return response.data
+    })
+}
+
+function updateTask(title, id){
+    const promise = axios.put(`https://repetitora.net/api/JS/Tasks?`,{
+        widgetId: 4242,
+        title: title,
+        taskId: id
+    })
+    return promise.then((response)=>{
+        return response.data
+    })
+}
+
+function deleteTask(id){
+    const promise = axios.delete(`https://repetitora.net/api/JS/Tasks?widgetId=4242&taskId=${id}`)
+    return promise.then((response)=>{
+        return response.data
     })
 }
